02788f5d8ac41ab82a58301ea6a2f3d9f8911e51
scottbedard <scottbedard90@gmail.com>
2016-03-27 21:46:53 -0700

    Add route to leave message

---
+++ http/MessagesController.php
@@ -0,0 +1,17 @@
+<?php namespace BeEasy\Contact\Http;
+
+use Illuminate\Routing\Controller;
+
+class MessagesController extends Controller
+{
+
+    /**
+     * Leave a message
+     *
+     * @return string
+     */
+    public function create()
+    {
+        return 'Hello from the messages create method';
+    }
+}
---
+++ routes.php
@@ -0,0 +1,3 @@
+<?php
+
+Route::post('api/beeasy/messages', 'BeEasy\BlogExtension\Controllers\PostsController@create');
